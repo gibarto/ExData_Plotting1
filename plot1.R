dbf329105b0bd79390bd266deb8bc47459d89564
gibarto <bart.geoff@yahoo.com>
2014-06-08 14:00:10 -0700

    plot 1 update

--- plot1.R
+++ plot1.R
@@ -11,7 +11,7 @@ rm(hpc)
 hpcS$Global_active_power<-as.numeric(hpcS$Global_active_power)
 
 ## make plot in PNG form
-png(filename="plot01.png", width=480, height=480, units="px")
+png(filename="plot1.png", width=480, height=480, units="px")
 hist(hpcS$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts", ylab="Frequency", col="red", axes=FALSE)
 axis(1, at=c(0,2,4,6))
 axis(2, at=c(0,200,400,600,800,1000,1200))
